8aef964fa4d08b88c07270d88950ef017f9743e2
RNO <arnaud.juracek@gmail.com>
2021-12-31 18:54:53 +0100

    Improve typographic rules, handle languages

--- server/templates/document.js
+++ server/templates/document.js
@@ -2,6 +2,10 @@ const fs = require('fs-extra')
 const path = require('path')
 const YAML = require('yaml')
 const markdownToc = require('markdown-toc')
+const anchor = require('markdown-it-anchor')
+const Markdown = require('markdown-it')
+const HLJS = require('highlight.js')
+
 const template = require('handlebars').compile(
   fs.readFileSync(
     path.join(__dirname, '..', '..', 'src', 'templates', 'document.hbs'),
@@ -9,66 +13,39 @@ const template = require('handlebars').compile(
   )
 )
 
-const anchor = require('markdown-it-anchor')
-const HLJS = require('highlight.js')
-const Markdown = require('markdown-it')({
-  html: true,
-  linkify: true,
-  typographer: true,
-  quotes: ['«\xA0', '\xA0»', '‹\xA0', '\xA0›'],
-  langPrefix: '',
-  highlight: (code, lang) => HLJS.highlight(code, {
-    language: HLJS.getLanguage(lang) ? lang : 'plaintext'
-  }).value
-}).use(require('markdown-it-abbr'))
-  .use(anchor, {
+const QUOTES = {
+  fr: ['«\xA0', '\xA0»', '‹\xA0', '\xA0›'],
+  en: '“”‘’'
+}
+
+const PLUGINS = [
+  [anchor, {
     level: 1,
     slugify: string => markdownToc.slugify(string.replace(/\u00A0/g, ' ')),
     permalink: anchor.permalink.linkInsideHeader({
       symbol: '¶',
       class: 'anchor'
     })
-  })
-  .use(require('markdown-it-emoji'))
-  .use(require('markdown-it-attribution'), {
+  }],
+  require('markdown-it-abbr'),
+  require('markdown-it-emoji'),
+  require('markdown-it-footnote'),
+  require('markdown-it-mark'),
+  [require('markdown-it-attribution'), {
     marker: '--',
     classNameContainer: 'blockquote-with-attribution',
     classNameAttribution: null
-  })
-  .use(require('markdown-it-footnote'))
-  .use(require('markdown-it-mark'))
+  }]
+]
 
 module.exports = async (resource, url) => {
-  const file = await fs.readFile(resource, 'utf8')
-  const metadata = await module.exports.readMetadata(resource, file)
+  let file = await fs.readFile(resource, 'utf8')
 
-  let html = template({
-    isProduction: process.env.NODE_ENV !== 'development',
-    markdown: Markdown.render(file
-      // Widont on headings
-      .replace(/^(#{1,6}\s.*) +(\W+)$/gmi, '$1&nbsp;$2')
-      .replace(/^(#{1,6}\s.*) +([^`\s]+)$/gmi, '$1&nbsp;$2')
-      // Remove front matter block
-      .replace(/^---(.|\n|\r)*?---/, '')
-    ),
-    toc: markdownToc(file).json.map(entry => {
-      // Remove footnotes
-      entry.content = entry.content.replace(/\[\^\d+\]/g, '')
-      entry.markdown = Markdown.render(entry.content)
-      entry.slug = markdownToc.slugify(entry.content)
-      return entry
-    }),
-    ...metadata
-  })
-
-  html = html
-    // Always render sup/sub inline when this is the only element on the line
-    .replace(/<p>(<sup>.*<\/sup>)<\/p>/gi, '$1')
-    .replace(/<p>(<sub>.*<\/sub>)<\/p>/gi, '$1')
-    // Ensure [[discuss]] is not wrapped inside a <p>
-    .replace(/<p>(\[\[\s?discuss(:\w+)?\s?(readonly)?\s\]\])<\/p>/gi, '$1')
-    // Prevent floating punctuation
-    .replace(/([\w>])\s([:?!])/g, '$1&#8239;$2')
+  // Apply some rules on raw markdown
+  file = file
+    // Widont on headings
+    .replace(/^(#{1,6}\s.*) +(\W+)$/gmi, '$1\u00a0$2')
+    .replace(/^(#{1,6}\s.*) +([^`\s]+)$/gmi, '$1\u00a0$2')
     // Replace fractions
     .replace(/\b1\/2([^\d])/g, '&frac12;$1') // ½
     .replace(/\b1\/3([^\d])/g, '&frac13;$1') // ⅓
@@ -88,6 +65,51 @@ module.exports = async (resource, url) => {
     .replace(/\b7\/8([^\d])/g, '&frac78;$1') // ⅞
     .replace(/\b1\/9([^\d])/g, '&#2151;$1') // ⅑
     .replace(/\b1\/10([^\d])/g, '&#8530;$1') // ⅒
+    // Replace multiplication sign
+    .replace(/(\d+)\s?x\s?(\d+)/g, '$1\u00a0×\u00a0$2')
+
+  const metadata = await module.exports.readMetadata(resource, file)
+
+  const markdown = new Markdown({
+    html: true,
+    linkify: true,
+    typographer: true,
+    quotes: QUOTES[metadata.lang || 'en'],
+    langPrefix: '',
+    highlight: (code, lang) => HLJS.highlight(code, {
+      language: HLJS.getLanguage(lang) ? lang : 'plaintext'
+    }).value
+  })
+
+  for (const plugin of PLUGINS) {
+    markdown.use(...Array.isArray(plugin) ? plugin : [plugin])
+  }
+
+  let html = template({
+    isProduction: process.env.NODE_ENV !== 'development',
+    markdown: markdown.render(file
+      // Remove front matter block
+      .replace(/^---(.|\n|\r)*?---/, '')
+    ),
+    toc: markdownToc(file).json.map(entry => {
+      // Remove footnotes
+      entry.content = entry.content.replace(/\[\^\d+\]/g, '')
+      entry.markdown = markdown.render(entry.content)
+      entry.slug = markdownToc.slugify(entry.content)
+      return entry
+    }),
+    ...metadata
+  })
+
+  // Apply some rules on rendered markdown
+  html = html
+    // Always render sup/sub inline when this is the only element on the line
+    .replace(/<p>(<sup>.*<\/sup>)<\/p>/gi, '$1')
+    .replace(/<p>(<sub>.*<\/sub>)<\/p>/gi, '$1')
+    // Ensure [[discuss]] is not wrapped inside a <p>
+    .replace(/<p>(\[\[\s?discuss(:\w+)?\s?(readonly)?\s\]\])<\/p>/gi, '$1')
+    // Prevent floating punctuation
+    .replace(/([\w>])\s([:?!])/g, '$1&#8239;$2')
 
   // Replace [[discuss]] by an HTML container for its jsx Component
   const logFile = resource.replace(metadata.extension, '.log')
